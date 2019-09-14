628da9a10b8a94f17d7f101cbc6e36a63bf22e6f
almbr <erik.almbratt@gmail.com>
2019-09-14 21:18:09 +0200

    Dont use git worktree for publish

--- publish.sh
+++ publish.sh
@@ -1,30 +1,19 @@
 #!/bin/sh
 
-if [ "`git status -s`" ]
-then
-    echo "The working directory is dirty. Please commit any pending changes."
-    exit 1;
-fi
-
 echo "Deleting old publication"
 rm -rf public
-mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
-git branch -D deploy
-
-echo "Checking out gh-pages branch into public"
-git worktree add -B deploy public origin/deploy
-
-echo "Removing existing files"
-rm -rf public/*
 
 echo "Generating site"
 hugo
 
-echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to deploy (publish.sh)"
+git branch -D deploy
 
-echo "Pushing to github"
-git push --all
+git checkout --orphan deploy
+git rm -r --cached
+git add -f ./public 
+git mv ./public/* ./
 
+git clean -fd
+
+git commit -m "Deploy"
+git push --force --set-upstream origin deploy
