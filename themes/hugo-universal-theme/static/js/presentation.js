525c44a23761961024653a4df09264127c1d8e7f
svirvel <alice@alicenilsson.com>
2019-12-08 19:43:23 +0100

    Clear auction presentation input box on enter Clear the item id input box on enter key. This should make it possible to automatic update the item currently viewed when using a barcode scanner. Also setting automatic focus on the text box.

--- themes/hugo-universal-theme/static/js/presentation.js
+++ themes/hugo-universal-theme/static/js/presentation.js
@@ -1,32 +1,37 @@
 jQuery(document).ready(function(){
 
+  // Set focus on the item-id textbox
+  $('#presentation-search-input').focus();
+
   $('#presentation-search-input').on('keypress', function (e) {
     $('#presentation-search-input').removeAttr('style');
 
-   if(e.which === 13){
-     if($('#presentation-search-input').val() == "") {
-       $('#presentation-item').hide();
-       $('#presentation-welcome').show();
-       $('#presentation-search-input').removeAttr('style');
-     } else {
-       $('#presentation-welcome').hide();
+    // Check if enter key is pressed
+    if(e.which === 13){
 
-       $.getJSON("/auction/presentation/index.json", function(data) {
+      // Hide the welcome message
+     $('#presentation-welcome').hide();
 
-         var item = data.items.find(item => item.id === $('#presentation-search-input').val());
+     // Load the json catalogue
+     $.getJSON("/auction/presentation/index.json", function(data) {
 
-         if(typeof item == 'undefined') {
-           $('#presentation-search-input').css("color","red");
-         } else {
-           $('#presentation-item-title').text(item.title);
-           $('#presentation-item-description').text(item.description);
-           $('#presentation-item-img').attr("src",item.image);
-           $('#presentation-item-status').text(item.status);
-           $('#presentation-item').show();
-         }
-       });
-     }
-   }
- });
+       // Search for the catalogue item
+       var item = data.items.find(item => item.id === $('#presentation-search-input').val());
 
+       // Check if an item matching the was found
+       if(typeof item !== 'undefined') {
+         // Update the item information
+         $('#presentation-item-title').text(item.title);
+         $('#presentation-item-description').text(item.description);
+         $('#presentation-item-img').attr("src",item.image);
+         $('#presentation-item-status').text(item.status);
+         $('#presentation-item').show();
+       }
+
+       // Clear the item-id input from any text
+       $('#presentation-search-input').val("");
+
+     });
+    }
+  });
 });
